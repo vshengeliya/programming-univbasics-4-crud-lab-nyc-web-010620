e7f8af31ec03dcab707387cef6f01ca6213e7ccf
bright-format-7059 <v.shengeliya@gmail.com>
2019-11-13 00:47:05 +0000

    Done.

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,39 +1,57 @@
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
+  ["cat", "dog", "bird", "tiger"]
+end
+
+def add_element_to_end_of_array(array, element)=
+ ["cat", "dog", "bird", "tiger"]
+ add_element_to_end_of_array.push("lion")
+ p add_element_to_end_of_array
+end
+
+def add_element_to_start_of_array(array, element)=
+  ["cat", "dog", "bird", "tiger"]
+  add_element_to_start_of_array.unshift("lion")
+  p add_element_to_start_of_array
+end
+
+def remove_element_from_end_of_array(array) = 
+["cat", "dog", "bird", "tiger"]
+tiger_el = remove_element_from_end_of_array.pop
+p remove_element_from_end_of_array
+end
+
+def remove_element_from_start_of_array(array) = 
+["cat", "dog", "bird", "tiger"]
+cat_el = remove_element_from_start_of_array.shift
+p remove_element_from_start_of_array
+end
+
+def retrieve_element_from_index(array, index_number) =
+  ["cat", "dog", "bird", "tiger"]
+  retrieve_element_from_index[]
   
 end
 
-def add_element_to_end_of_array(array, element)
-  
-end
-
-def add_element_to_start_of_array(array, element)
-  
-end
-
-def remove_element_from_end_of_array(array)
-  
-end
-
-def remove_element_from_start_of_array(array)
-  
-end
-
-def retrieve_element_from_index(array, index_number)
-  
-end
-
-def retrieve_first_element_from_array(array)
+def retrieve_first_element_from_array(array)=
+    ["cat", "dog", "bird", "tiger"]
+  retrieve_first_element_from_array[0]
   
 end
 
 def retrieve_last_element_from_array(array)
+     ["cat", "dog", "bird", "tiger"]
+  retrieve_last_element_from_array[-1]
   
 end
 
-def update_element_from_index(array, index_number, element)
+def update_element_from_index(array, index_number, element)=
+  ["cat", "dog", "bird", "tiger"]
+  update_element_from_index[1] = "monkey"
+  =>"monkey"
+  p update_element_from_index
 
 end
