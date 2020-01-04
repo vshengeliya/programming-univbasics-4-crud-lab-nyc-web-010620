9ebb1c641ffdb63af64753b0a8ee49431c2c9da2
Veronika Dodda <v.shengeliya@gmail.com>
2020-01-04 04:06:15 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -10,7 +10,7 @@ def add_element_to_end_of_array(array, element)
 array.push (element)
 end
 
-def add_element_to_end_of_array(array, element)
+def add_element_to_start_of_array(array, element)
 array.unshift (element)
 end
 
