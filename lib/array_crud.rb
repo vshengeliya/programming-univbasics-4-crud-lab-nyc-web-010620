1aa01535cf348d7586128315b31226de599c82f7
Veronika Dodda <v.shengeliya@gmail.com>
2020-01-04 04:03:39 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-array["cat", "dog", "bird", "tiger"].push ("lino")
+array["cat", "dog"].push ("lion")
 end
 
 # def add_element_to_start_of_array(array, element)=
