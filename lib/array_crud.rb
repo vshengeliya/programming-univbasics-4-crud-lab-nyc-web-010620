17d1b1c6e7afcacdbcadb5748d380d94111a4a36
Veronika Dodda <v.shengeliya@gmail.com>
2020-01-04 04:06:49 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -14,8 +14,8 @@ def add_element_to_start_of_array(array, element)
 array.unshift (element)
 end
 
-def add_element_to_start_of_array(array, element)
-array.unshift (element)
+def remove_element_from_end_of_array(array, element)
+array.pop (element)
 end
 
 
